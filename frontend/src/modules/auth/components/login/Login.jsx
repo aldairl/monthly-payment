ba2374a7ad91b55a9df80579ecd7c15ac7a14cb3
aldairl <aluna@yeapp.io>
2025-02-13 07:01:57 -0500

    feat: login ui

--- frontend/src/modules/auth/components/login/Login.jsx
+++ frontend/src/modules/auth/components/login/Login.jsx
@@ -1,5 +1,70 @@
-export const Login = () => {
-  return (
-    <div>Login</div>
-  )
+import PropTypes from 'prop-types'
+import { Box, Button, TextField } from "@mui/material"
+import { Form, Formik } from "formik"
+
+export const Login = ({ isNonMobile, handleFormSubmit, initialValues, checkoutSchema, }) => {
+    return (
+        <Box>
+            <h1>Bienvenido</h1>
+            <h3>Iniciar sesión</h3>
+            <Formik
+                onSubmit={handleFormSubmit}
+                initialValues={initialValues}
+                validationSchema={checkoutSchema}
+            >
+                {({ values, errors, touched, handleBlur, handleChange, handleSubmit, }) => (
+                    <Form onSubmit={handleSubmit}>
+                        <Box
+                            display="grid"
+                            gap="30px"
+                            gridTemplateColumns="repeat(4, minmax(0, 1fr))"
+                            sx={{
+                                "& > div": { gridColumn: isNonMobile ? undefined : "span 4" },
+                            }}
+                        >
+                            <TextField
+                                fullWidth
+                                variant="filled"
+                                type="text"
+                                label="Usuario"
+                                onBlur={handleBlur}
+                                onChange={handleChange}
+                                value={values.username}
+                                name="username"
+                                error={!!touched.username && !!errors.username}
+                                helperText={touched.username && errors.username}
+                                sx={{ gridColumn: "span 4" }}
+                            />
+                            <TextField
+                                fullWidth
+                                variant="filled"
+                                type="password"
+                                label="Contraseña"
+                                onBlur={handleBlur}
+                                onChange={handleChange}
+                                value={values.password}
+                                name="password"
+                                error={!!touched.password && !!errors.password}
+                                helperText={touched.password && errors.password}
+                                sx={{ gridColumn: "span 4" }}
+                            />
+                        </Box>
+
+                        <Box display="flex" justifyContent="center" mt="20px">
+                            <Button type="submit" color="secondary" variant="contained">
+                                Aceptar
+                            </Button>
+                        </Box>
+                    </Form>
+                )}
+            </Formik>
+        </Box>
+    )
+}
+
+Login.propTypes = {
+    isNonMobile: PropTypes.bool,
+    handleFormSubmit: PropTypes.func,
+    initialValues: PropTypes.object,
+    checkoutSchema: PropTypes.object,
 }
--- frontend/src/modules/auth/components/login/LoginContainer.jsx
+++ frontend/src/modules/auth/components/login/LoginContainer.jsx
@@ -1,7 +1,32 @@
+import useMediaQuery from "@mui/material/useMediaQuery"
+import * as yup from "yup"
 import { Login } from './Login'
 
+
+const checkoutSchema = yup.object().shape({
+    username: yup.string().required("required"),
+    password: yup.string().required("required"),
+})
+
+const initialValues = {
+    username: "",
+    password: "",
+}
+
 export const LoginContainer = () => {
-  return (
-    <Login/>
-  )
+
+    const isNonMobile = useMediaQuery("(min-width:600px)")
+
+    const handleFormSubmit = (values) => {
+        console.log(values)
+    }
+
+    return (
+        <Login
+            isNonMobile={isNonMobile}
+            handleFormSubmit={handleFormSubmit}
+            initialValues={initialValues}
+            checkoutSchema={checkoutSchema}
+        />
+    )
 }
