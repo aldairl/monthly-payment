c7e06b36632ef8b113dbea428764ccd26e8d2732
aldairl <aluna@yeapp.io>
2025-02-16 23:50:18 -0500

    feat: add beneficiary form

--- frontend/src/modules/dashboard/components/user/components/addUser/AddUserContainer.jsx
+++ frontend/src/modules/dashboard/components/user/components/addUser/AddUserContainer.jsx
@@ -1,7 +1,44 @@
+import { useDispatch, useSelector } from 'react-redux'
 import { AddUser } from './AddUser'
+import * as yup from "yup"
+import { addBeneficiary } from '../../storage/beneficiaryThunks'
+
+const checkoutSchema = yup.object().shape({
+  name: yup.string().required("required"),
+  lastname: yup.string(),
+  identification: yup.string().required("required"),
+  birthday: yup.date(),
+  temple: yup.string().required("required"),
+  cellphone: yup.string().required("required"),
+})
+
+const initialValues = {
+  name: '',
+  lastname: '',
+  identification: '',
+  birthday: '',
+  temple: '',
+  cellphone: '',
+}
 
 export const AddUserContainer = () => {
+
+  const dispatch = useDispatch()
+  const { loading, error, beneficiarySelected } = useSelector(state => state.beneficiary)
+
+  const handleFormSubmit = (values) => {
+    console.log(values)
+    dispatch(addBeneficiary(values))
+  }
+
   return (
-    <AddUser />
+    <AddUser
+      checkoutSchema={checkoutSchema}
+      initialValues={initialValues}
+      handleFormSubmit={handleFormSubmit}
+      loading={loading}
+      error={error}
+      beneficiarySelected={beneficiarySelected}
+    />
   )
 }
